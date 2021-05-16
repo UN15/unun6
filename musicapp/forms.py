5d8d610beb70387100b363f4dd1e64823d9234d9
unun2 <jaeun101501@gmail.com>
2021-05-16 20:21:12 +0900

    music

--- musicapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Musicapp
-
-class MusicappForm(forms.ModelForm):
-    class Meta:
-        model = Musicapp
-        fields = ['title', 'writer', 'body']
-
--- musicapp2/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Musicapp2
-
-class Musicapp2Form(forms.ModelForm):
-    class Meta:
-        model = Musicapp2
-        fields = ['title', 'writer', 'body', 'image']
-
